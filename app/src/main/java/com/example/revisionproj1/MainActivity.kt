0cb5c092ca512f124631d20717e4f7fd46b410f5
akashkroy69 <akashkroy@hotmail.com>
2024-02-20 01:18:37 +0530

    practicing text preservation using Bundle,(getting errors)

--- app/src/main/java/com/example/revisionproj1/MainActivity.kt
+++ app/src/main/java/com/example/revisionproj1/MainActivity.kt
@@ -4,79 +4,38 @@ import android.os.Bundle
 import android.os.SystemClock
 import android.widget.Button
 import android.widget.Chronometer
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var stopwatch:Chronometer
-    private var running = false
-    private var offset:Long = 0
-
-//    constants
-    private val OFFSET_VALUE = "offset"
-    private val RUNNING_STATUS = "running"
-    private val BASE = "base"
+    private val TEXTKEY = "text"
+    private val editText: EditText = findViewById(R.id.editTextId)
+    private val textView:TextView = findViewById(R.id.displayTextId)
+    private val button:Button = findViewById<Button>(R.id.displayButtonId)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        stopwatch = findViewById<Chronometer>(R.id.chronometer)
 
-        if(savedInstanceState != null){
-            offset = savedInstanceState.getLong(OFFSET_VALUE)
-            running = savedInstanceState.getBoolean(RUNNING_STATUS)
-            if (running){
-                stopwatch.base = savedInstanceState.getLong(BASE)
-                stopwatch.start()
-            }else{
-                setBaseTime()
-            }
+        if (savedInstanceState != null){
+            textView.text = savedInstanceState.getString(TEXTKEY)
         }
 
-        val startButton = findViewById<Button>(R.id.start)
-        val pauseButton = findViewById<Button>(R.id.pause)
-        val resetButton = findViewById<Button>(R.id.reset)
-
-        startButton.setOnClickListener {
-            if(!running) {
-                running = true
-                setBaseTime()
-                stopwatch.start()
-            }
-
-        }
-
-        pauseButton.setOnClickListener {
-            if(running){
-                saveOffset()
-                running = false
-                stopwatch.stop()
-
-            }
-
-        }
-
-        resetButton.setOnClickListener {
-            offset = 0
-            stopwatch.base = SystemClock.elapsedRealtime()-offset
-
+        button.setOnClickListener {
+            textView.text = editText.text
         }
 
         }
-
-     private fun setBaseTime(){
-         stopwatch.base = SystemClock.elapsedRealtime() - offset
-     }
-
-    private fun saveOffset(){
-        offset = SystemClock.elapsedRealtime() - stopwatch.base
-    }
 
     override fun onSaveInstanceState(outState: Bundle) {
-        outState.putLong(OFFSET_VALUE,offset)
-        outState.putBoolean(RUNNING_STATUS,running)
-        outState.putLong(BASE,stopwatch.base)
         super.onSaveInstanceState(outState)
+        outState.putString(TEXTKEY,textView.text.toString())
     }
+
+
+
+
 
 
 
