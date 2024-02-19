8d34c1973900eba61791f058dd1287a848d02c52
akashkroy69 <akashkroy@hotmail.com>
2024-02-20 01:38:32 +0530

    Bundle practie done, WORKING

--- app/src/main/java/com/example/revisionproj1/MainActivity.kt
+++ app/src/main/java/com/example/revisionproj1/MainActivity.kt
@@ -10,33 +10,31 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     private val TEXTKEY = "text"
-    private val editText: EditText = findViewById(R.id.editTextId)
-    private val textView:TextView = findViewById(R.id.displayTextId)
-    private val button:Button = findViewById<Button>(R.id.displayButtonId)
+    private var textValue: CharSequence? = "***"
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val editText: EditText = findViewById(R.id.editTextId)
+        val textView:TextView = findViewById(R.id.displayTextId)
+        val button:Button = findViewById(R.id.displayButtonId)
+
         if (savedInstanceState != null){
-            textView.text = savedInstanceState.getString(TEXTKEY)
+            textView.text = savedInstanceState.getCharSequence(TEXTKEY)
         }
 
         button.setOnClickListener {
             textView.text = editText.text
+            textValue = textView.text
         }
 
         }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        outState.putString(TEXTKEY,textView.text.toString())
+        outState.putCharSequence(TEXTKEY,textValue)
     }
-
-
-
-
-
-
 
     }
