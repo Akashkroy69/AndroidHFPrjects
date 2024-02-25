ced62ce1b91124771f80a386f56f05c35a9222ff
akashkroy69 <akashkroy@hotmail.com>
2024-02-25 18:23:08 +0530

    initial setup for navigation ui components

--- app/src/main/java/com/example/revisionproj1/MainActivity.kt
+++ app/src/main/java/com/example/revisionproj1/MainActivity.kt
@@ -1,11 +1,6 @@
 package com.example.revisionproj1
 
 import android.os.Bundle
-import android.text.Editable
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Spinner
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -13,20 +8,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        ids of views
-        val findBeerButton = findViewById<Button>(R.id.okButtonId)
-        val beerText = findViewById<TextView>(R.id.beerTypeTextId)
-        val spinner = findViewById<Spinner>(R.id.spinnerId)
-        val inputArea = findViewById<EditText>(R.id.inputTextId)
-        findBeerButton.setOnClickListener {
-//          val beer = spinner.selectedItem.toString() or following will work
-            val beer = "${spinner.selectedItem}"
-            beerText.text = beer
-//            in the input area string is not allowed, but the editable  is allowed
-            inputArea.text = Editable.Factory.getInstance().newEditable(beer)
 
         }
 
 
-    }
 }
