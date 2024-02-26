d6ec886b3c7221f232fc282282887e70ab5c3321
akashkroy69 <akashkroy@hotmail.com>
2024-02-26 12:33:15 +0530

    customized toolbar set up

--- app/src/main/java/com/example/revisionproj1/MainActivity.kt
+++ app/src/main/java/com/example/revisionproj1/MainActivity.kt
@@ -2,12 +2,16 @@ package com.example.revisionproj1
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.appbar.MaterialToolbar
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//        adding the MaterialToolbar as app bar
+        val toolbar = findViewById<MaterialToolbar>(R.id.toolbarId)
+        setSupportActionBar(toolbar)
 
         }
 
