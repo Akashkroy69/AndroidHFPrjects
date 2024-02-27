db384408d4508ed85b19c7b8cbc7af4ab46fdc7c
akashkroy69 <akashkroy@hotmail.com>
2024-02-27 15:26:42 +0530

    saving work, menu added, code not working

--- app/src/main/java/com/example/revisionproj1/MainActivity.kt
+++ app/src/main/java/com/example/revisionproj1/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.revisionproj1
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.onNavDestinationSelected
 import com.google.android.material.appbar.MaterialToolbar
 
 class MainActivity : AppCompatActivity() {
@@ -15,5 +19,14 @@ class MainActivity : AppCompatActivity() {
 
         }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val navController = findNavController(R.id.fragmentContainerViewId)
+        return item.onNavDestinationSelected(navController)|| super.onOptionsItemSelected(item)
+    }
 
 }
